09add370b977e2eb8a023c95428174938fd0aeae
Vidya <vidya.kushte@gmail.com>
2025-01-02 19:59:45 -0600

    config readere final

--- src/test/java/dsalgo_utils/ConfigReader.java
+++ src/test/java/dsalgo_utils/ConfigReader.java
@@ -1,7 +1,6 @@
 package dsalgo_utils;
 
 	import java.io.BufferedReader;
-	import java.io.FileInputStream;
 	import java.io.FileNotFoundException;
 	import java.io.FileReader;
 	import java.io.IOException;
@@ -46,6 +45,14 @@ package dsalgo_utils;
 				return path;
 			else
 				throw new RuntimeException("url not specified in the Configuration.properties file.");
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 
